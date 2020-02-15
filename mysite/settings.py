f67075de4eb4b29c02021a19729fa0e89b5a4937
sdalabehera <salinkumar143@gmail.com>
2020-02-15 11:51:57 +0530

    finall

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqllite3',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
         #'USER': 'sdalabehera',
         #'PASSWORD':'',
